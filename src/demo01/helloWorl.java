4e839ccee3904fbf37cfc1d08b426e2a3204bd43
har213 <sharnoor213@gmail.com>
2025-09-15 19:38:38 -0400

    added name

--- src/demo01/helloWorl.java
+++ src/demo01/helloWorl.java
@@ -2,6 +2,8 @@ package demo01;
 
 public class helloWorl {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		String name = "Har";
+		System.out.println("Hello" + name);
+		
 	}
 }	
